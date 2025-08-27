2e2fb2510dd033a6c8582caab64bbd33e7f85603
vIadimirp <vladimirp.main@gmail.com>
2025-08-27 13:45:20 +0300

    v1.1.0 commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let millisecondsVisible = true;
+let scrollbarVisible = true;
+
 function toLength(s, l) {
     return '0'.repeat(l - s.toString().length) + s.toString();
 }
@@ -19,22 +22,74 @@ function fillLabels() {
     document.querySelector('span#time-ms').innerHTML = '.' + dateTimeArray[6][0];
     document.querySelector('span#date').innerHTML = dateTimeArray[0] + '-' + dateTimeArray[1] + '-' + dateTimeArray[2];
 }
+
+
 function changeBgColor() {
-    document.querySelector('html').style.background = document.querySelector('input#bg-color-selector').value;
+    if (document.querySelector('input#background-color-radio').checked) {
+        document.querySelector('html').style.background = document.querySelector('input#background-color-selector').value;
+    }
 }
-function changeBgImage() {
-    document.querySelector('html').style.background = 'url(' + document.querySelector('input#bg-img-selector').value + ')';
-    alert('url(' + document.querySelector('input#bg-img-selector').value + ')');
+function changeBgGradient() {
+    const degreesValue = document.querySelector('input#bg-gradient-selector-deg').value;
+    const degreesInput = document.querySelector('input#bg-gradient-selector-deg');
+    const color_1 = document.querySelector('input#bg-gradient-selector-1').value;
+    const color_2 = document.querySelector('input#bg-gradient-selector-2').value;
+    if (document.querySelector('input#background-gradient-radio').checked) {
+        if (degreesValue > 360) {
+            degreesInput.value = 360;
+        } else if (degreesValue < 0) {
+            degreesInput.value = 0;
+        } else if (degreesValue === '') {
+            degreesInput.value = '0';
+        }
+        document.querySelector('html').style.background = 'linear-gradient(' + degreesValue + 'deg, ' + color_1 + ', ' + color_2 + ')';
+    }
 }
+function changeBgImage() {}
 function changeFontColor() {
-    document.querySelector('div#datetime-container').style.color = document.querySelector('input#font-color-selector').value;
+    const value = document.querySelector('input#font-color-selector').value;
+    document.querySelector('div#datetime-container').style.color = value;
+    document.querySelector('svg').style.color = value;
 }
-function run() {
-    document.querySelector('input#bg-color-selector').value = document.querySelector('html').style.background;
-    document.querySelector('input#font-color-selector').value = document.querySelector('div#datetime-container').style.color;
-    document.querySelector('input#bg-color-selector').oninput = changeBgColor;
+function millisecondsToggle() {
+    const label = document.querySelector('span#time-ms');
+    if (millisecondsVisible) {
+        label.style.display = 'none';
+        millisecondsVisible = false;
+    } else if (!millisecondsVisible) {
+        label.style.display = 'inline';
+        millisecondsVisible = true;
+    }
+}
+function scrollbarToggle() {
+    const style = document.querySelector('head style#scrollbar-style');
+    if (scrollbarVisible) {
+        style.innerHTML = '::-webkit-scrollbar {display: none;}';
+        scrollbarVisible = false;
+    } else if (!scrollbarVisible) {
+        style.innerHTML = '';
+        scrollbarVisible = true;
+    }
+    
+}
+
+
+function startup() {
+    document.querySelector('input#background-color-selector').oninput = changeBgColor;
+    document.querySelector('input#background-color-radio').oninput = changeBgColor;
+    document.querySelector('input#bg-gradient-selector-deg').oninput = changeBgGradient;
+    document.querySelector('input#bg-gradient-selector-1').oninput = changeBgGradient;
+    document.querySelector('input#bg-gradient-selector-2').oninput = changeBgGradient;
+    document.querySelector('input#background-gradient-radio').oninput = changeBgGradient;
+    document.querySelector('input#background-image-radio').oninput = changeBgImage;
+    document.querySelector('input#background-image-selector').oninput = changeBgImage;
     document.querySelector('input#font-color-selector').oninput = changeFontColor;
-    document.querySelector('input#bg-img-selector').oninput = changeBgImage;
+    document.querySelector('input#show-ms-checkbox').oninput = millisecondsToggle;
+    document.querySelector('input#hide-scrollbar-checkbox').oninput = scrollbarToggle;
+    
     setInterval(fillLabels, 50);
 }
-run()
+function run() {
+    startup();
+}
+run();
